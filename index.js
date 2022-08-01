53051f0ab258093c449b99f0396d7f25fa86f649
gavand-sagar <gavand.sagar.s@gmail.com>
2022-08-01 19:17:24 +0530

    app v1

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+import express from "express";
+import cors from 'cors';
+import dotenv from 'dotenv'
+dotenv.config('./env')
+
+const app = express();
+app.use(express.json())
+app.use(cors())
+app.get('/',(req,res)=>{
+    res.send("hello")
+})
+app.listen(process.env.PORT, () => {
+    console.log('Server is listening on ' + process.env.PORT)
+})
 
